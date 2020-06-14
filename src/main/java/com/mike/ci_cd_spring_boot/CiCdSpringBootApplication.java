0c84008b47c9f4e1232e61d8b4a705313992570a
Mike <mihalich93@bk.ru>
2020-06-14 18:11:56 +0300

    initial commit

---
+++ src/main/java/com/mike/ci_cd_spring_boot/CiCdSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mike.ci_cd_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdSpringBootApplication.class, args);
+	}
+
+}
