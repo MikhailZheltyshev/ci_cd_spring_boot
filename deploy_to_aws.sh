07329b7b8e038695fe082159f8b5d77306af1b03
Mike <mihalich93@bk.ru>
2020-06-15 20:35:23 +0300

    switched to docker image deployment to ecr

---
+++ deploy_to_aws.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# install AWS SDK
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+
+# login to AWS ECR
+eval $(aws ecr get-login --region eu-north-1)
+
+# prepare docker image
+export TAG=$(if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo "$TRAVIS_BRANCH"; fi)
+export IMAGE_NAME=$ECR/ci-cd-spring-boot
+docker build -t $IMAGE_NAME:$TRAVIS_COMMIT .
+docker tag $IMAGE_NAME:$TRAVIS_COMMIT $IMAGE_NAME:$TAG
+
+# push image to the AWS ECR
+docker push $IMAGE_NAME
